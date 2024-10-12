df3cbf0d8f8defb1b53a6fb0656bb9f77e25cf87
murodbek_08 <murodbekarslonov201@gmail.com>
2024-10-13 00:02:31 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,70 @@
+// loading
+const loading = document.getElementById("loading");
+
+const loadingDuration = 2000; // 2s
+
+setTimeout(() => {
+  loading.classList.add("loading-none");
+}, loadingDuration);
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-mune");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-btn");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("activeBtn");
+  });
+});
+//bectop
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// navbar shrink
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("header");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
+
+// dark-mode
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "./images/sun.svg";
+  } else {
+    this.firstElementChild.src = "./images/moon.svg";
+  }
+  document.body.classList.toggle("dark");
+});
